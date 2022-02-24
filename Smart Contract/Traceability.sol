b958618589a0f05304d532f75f3995f3f743aa99
Christian Widjaja <christianwidjaja04@gmail.com>
2022-02-24 13:45:03 +0700

    Modifying Smart Contract
    Adding comments
    Modified Smart Contract function

--- Smart Contract/Traceability.sol
+++ Smart Contract/Traceability.sol
@@ -10,105 +10,108 @@ contract Traceability {
         owner = msg.sender;
     }
 
-    mapping(address => bool) public party1;
-    mapping(address => bool) public party2;
-    mapping(address => bool) public party3;
-    mapping(address => bool) public party4;
+    //modifier untuk mengecek owner
+    modifier onlyOwner() {
+        require(isOwner(), "You are not the owner!");
+        _;
+    }
 
-    event Trace(uint _itemID, uint _step, bool _halal);
-    event AddParty(address _address, bool _status);
+    //function untuk mengecek apakah yang ingin memanggil function adalah owner
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner;
+    }
+
+    modifier itemHalal(uint _itemID) {
+        require(isHalal(_itemID), "Item is not Halal! can't proceed!");
+        _;
+    }
+
+    function isHalal(uint _itemID) public view returns(bool _halal){
+        return items[_itemID].halal;
+    } 
+
+    
+    event Trace(uint _itemID, uint _step, bool _halal, string verifiers, uint _time);
     
     enum SupplyChainSteps{Step1, Step2, Step3, Step4}
 
+
+    //Struct untuk item yang akan dipantau
     struct Item{
+        //Step supply chain berdasarkan traceability dari item
         Traceability.SupplyChainSteps step;
+
+        //Id dari item
         uint id;
+
+        //Nama dari item
         string name;
+
+        //Status halal dari item
         bool halal;
+
+        //Nama dari penginput data
+        mapping(uint => string) verifiers;
     }
 
+    //mapping untuk menyimpan informasi item;
     mapping(uint => Item) public items;
+
+    //index sebagai id item;
     uint index;
 
-    function createItem(string memory _name, bool _halal) public{
-        require(party1[msg.sender], "You are not allowed to use this function");
+    //function untuk membuat item
+    function createItem(string memory _name, string memory _verifier, bool _halal) public onlyOwner{
 
+        //function untuk menambahkan informasi item
         items[index].id = index;
         items[index].step = SupplyChainSteps.Step1;
         items[index].name = _name;
         items[index].halal = _halal;
+        items[index].verifiers[0] = _verifier;
 
-        emit Trace(items[index].id, uint(items[index].step), items[index].halal);
 
+        emit Trace(items[index].id, uint(items[index].step), items[index].halal, items[index].verifiers[0], block.timestamp);
         index++;
     }
 
-    function step2(uint _itemID, bool _halal) public {
-        require(party2[msg.sender], "You are not allowed to use this function");
+    function step2(uint _itemID, string memory _verifier ,bool _halal) public  onlyOwner itemHalal(_itemID){
+        //mengecek apakah item sudah dibuat
         require(uint(items[_itemID].step) == 0, "Item does not exist");
-        require(items[_itemID].halal, "Item is not halal");
 
+        //mengupdate informasi item
         items[_itemID].step = SupplyChainSteps.Step2;
         items[_itemID].halal = _halal;
+        items[_itemID].verifiers[1] = _verifier;
 
-        emit Trace(items[_itemID].id, uint(items[_itemID].step), items[_itemID].halal);
+        emit Trace(items[_itemID].id, uint(items[_itemID].step), items[_itemID].halal, items[_itemID].verifiers[1], block.timestamp);
     }
 
     
-    function step3(uint _itemID, bool _halal) public {
-        require(party3[msg.sender], "You are not allowed to use this function");
+    function step3(uint _itemID, string memory _verifier ,bool _halal) public onlyOwner itemHalal(_itemID){
+        //mengecek apakah item sudah menjalankan proses sebelumnya
         require(uint(items[_itemID].step) == 1, "Item hasn't reached this step yet");
-        require(items[_itemID].halal, "Item is not halal");
 
+        //mengupdate informasi item
         items[_itemID].step = SupplyChainSteps.Step3;
         items[_itemID].halal = _halal;
+        items[_itemID].verifiers[2] = _verifier;
 
-        emit Trace(items[_itemID].id, uint(items[_itemID].step), items[_itemID].halal);
+
+        emit Trace(items[_itemID].id, uint(items[_itemID].step), items[_itemID].halal, items[_itemID].verifiers[2], block.timestamp);
     }
 
-    function step4(uint _itemID, bool _halal) public {
-        require(party4[msg.sender], "You are not allowed to use this function");
+    function step4(uint _itemID, string memory _verifier ,bool _halal) public onlyOwner itemHalal(_itemID){
+        //mengecek apakah item sudah menjalankan proses sebelumnya
         require(uint(items[_itemID].step) == 2, "Item hasn't reached this step yet");
-        require(items[_itemID].halal, "Item is not halal");
 
+        //mengupdate informasi item
         items[_itemID].step = SupplyChainSteps.Step4;
         items[_itemID].halal = _halal;
+        items[_itemID].verifiers[3] = _verifier;
 
-        emit Trace(items[_itemID].id, uint(items[_itemID].step), items[_itemID].halal);
+
+        emit Trace(items[_itemID].id, uint(items[_itemID].step), items[_itemID].halal, items[_itemID].verifiers[3], block.timestamp);
     }
-
-    function addParty1(address _party1) public {
-        require(msg.sender == owner, "Only owner can add new party");
-        party1[_party1] = true;
-
-        emit AddParty(_party1, party1[_party1]);
-    }
-
-    function addParty2(address _party2) public {
-        require(msg.sender == owner, "Only owner can add new party");
-        party2[_party2] = true;
-
-        emit AddParty(_party2, party2[_party2]);
-    }
-
-    function addParty3(address _party3) public {
-        require(msg.sender == owner, "Only owner can add new party");
-        party3[_party3] = true;
-
-        emit AddParty(_party3, party3[_party3]);
-    }
-
-    function addParty4(address _party4) public {
-        require(msg.sender == owner, "Only owner can add new party");
-        party4[_party4] = true;
-
-        emit AddParty(_party4, party4[_party4]);
-    }
-
-    function isHalal(uint _itemID) public view returns(uint _steps, bool _halal){
-        _steps = uint(items[_itemID].step);
-        _halal = items[_itemID].halal;
-    } 
-
     
 }
