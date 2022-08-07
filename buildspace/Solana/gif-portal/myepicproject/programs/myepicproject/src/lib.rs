b6545ab33daba40da78de6fb2fc59efb1b60b216
ChristianTPW <49968741+ChristianTPW@users.noreply.github.com>
2022-08-07 20:19:29 +0700

    Deploy to devnet

--- buildspace/Solana/gif-portal/myepicproject/programs/myepicproject/src/lib.rs
+++ buildspace/Solana/gif-portal/myepicproject/programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2uV52wYzEVMSHmENiwKFdYnmvxcxrjzXBKNgEWxJAyMm");
 
 #[program]
 pub mod myepicproject {
